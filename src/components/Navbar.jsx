1789b03f263c31e1a8a1b108c3014db594313dc0
Cathy V. Amamangpang <159250691+cathyang2@users.noreply.github.com>
2025-08-21 07:19:28 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 // src/components/Navbar.jsx
 import React, { useState } from "react";
 import "./Navbar.css";
-
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,8 +11,8 @@ export default function Navbar() {
       <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
-        <li><a href="/services">Login</a></li>
-        <li><a href="/contact">Sign Up</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">Sign Up</a></li>
       </ul>
 
       <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
@@ -24,5 +23,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-
